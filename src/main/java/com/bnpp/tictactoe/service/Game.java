154c9fceb26931c3275207b634b75afb5c0342c4
desaivishal42 <desaivishal42@gmail.com>
2021-05-16 00:13:49 +0530

    Validate input and throw exception in case input position already filled by other player

--- src/main/java/com/bnpp/tictactoe/service/Game.java
+++ src/main/java/com/bnpp/tictactoe/service/Game.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 public class Game {
 
     private static final String INVALID_INPUT_RANGE_ERROR = "Input should be [1-9]";
+    private static final String INVALID_INPUT_ALREADY_FILLED_ERROR = "Input position already filled by counter player";
+    private static final String X_INPUT = "X";
+    private static final String O_INPUT = "O";
 
     public GameBoard initBoard() {
         return new GameBoard();
@@ -19,7 +22,35 @@ public class Game {
         }
     }
 
-    public void validateIfInputPosFilled(String[][] board, int input) {
+    public void validateIfInputPosFilled(String[][] board, int input) throws InvalidInputException {
+        String value = getValueFromBoard(board, input);
+        if (value.equals(X_INPUT) || value.equals(O_INPUT)) {
+            throw new InvalidInputException(INVALID_INPUT_ALREADY_FILLED_ERROR);
+        }
+    }
 
+    private String getValueFromBoard(String[][] board, int input) throws InvalidInputException {
+        switch (input) {
+            case 1:
+                return board[0][0];
+            case 2:
+                return board[0][1];
+            case 3:
+                return board[0][2];
+            case 4:
+                return board[1][0];
+            case 5:
+                return board[1][1];
+            case 6:
+                return board[1][2];
+            case 7:
+                return board[2][0];
+            case 8:
+                return board[2][1];
+            case 9:
+                return board[2][2];
+            default:
+                throw new InvalidInputException(INVALID_INPUT_RANGE_ERROR);
+        }
     }
 }
