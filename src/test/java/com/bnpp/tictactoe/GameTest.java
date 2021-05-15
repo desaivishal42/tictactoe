d4467eaaa30fb03ef3201141ca2962c8c6dd7161
desaivishal42 <desaivishal42@gmail.com>
2021-05-15 22:55:12 +0530

    Add parameterized test to validate input by player

--- src/test/java/com/bnpp/tictactoe/GameTest.java
+++ src/test/java/com/bnpp/tictactoe/GameTest.java
@@ -5,6 +5,8 @@ import com.bnpp.tictactoe.service.Game;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,9 +24,9 @@ public class GameTest {
         Assertions.assertNotNull(gameBoard);
     }
 
-    @Test
-    public void shouldBeValidInputByPlayer() {
-        var input = 1;
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9})
+    public void shouldBeValidInputByPlayer(int input) {
         Assertions.assertDoesNotThrow(() -> game.validateInput(input));
     }
 }
